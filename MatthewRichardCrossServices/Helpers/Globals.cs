9c92ad200e3e2d6da59f4b8dd0085d51e6876b12
Edward G Trenchard <ed_trench@yahoo.com>
2012-09-18 17:09:25 +0100

    18-09-2012

--- MatthewRichardCrossServices/Helpers/Globals.cs
+++ MatthewRichardCrossServices/Helpers/Globals.cs
@@ -8,7 +8,7 @@ namespace MatthewRichardCrossServices.Helpers
     public class Globals
     {
         //Matthew Richard Cross Email
-        public const String MRC_Email_Address = "mattcross101@hotmail.com,ed_trench@yahoo.com";
+        public const String MRC_Email_Address = "ed_trench@yahoo.com,info@mcpropertyservices.net";
         //MailGun
         public const String MailGun_API_URL = "https://api.mailgun.net/v2";
         public const String MailGun_API_Key = "key-4mdd19leufnpvx-ouuiqp85ct2-gcw39";
